c37e5af4fee0b60c6c7b64ae21b4799af6c5bf9c
cij11 <cij11@uclive.ac.nz>
2017-04-12 20:01:23 +1200

    Updating server in real time, emitting player position to client, updating client canvas on receiving player position

--- app.js
+++ app.js
@@ -16,16 +16,27 @@ app.use('/client', express.static(__dirname + '/client'));
 serv.listen(2002);
 console.log("Server started");
 
+var SOCKET_LIST = {};
 
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
+  socket.id = Math.random();
+  socket.x = 0;
+  socket.y = 0;
+  SOCKET_LIST[socket.id] = socket; //Store new socket in the socket list
+
   console.log('socket connection');
-
-  socket.on('happy', function(data){  //happy is the type of message. data is the second parameter to the sockets emit method
-    console.log('happy because ' + data.reason);
-  })
-
-  socket.emit('serverMsg', {
-      msg:'hello',
-  });
 });
+
+//Called 25 times a second
+setInterval(function(){
+  for(var i in SOCKET_LIST){ //Loop through all sockets, update position, emit position to that client
+    var socket = SOCKET_LIST[i];
+    socket.x++;
+    socket.y++;
+    socket.emit('newPosition', {
+      x:socket.x,
+      y:socket.y
+    });
+  }
+}, 1000/25);
